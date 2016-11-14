2e3ad8dc5a7ee28632b81653f3984ab9337dc1af
Anthony Ricard <AnthonyRcd@users.noreply.github.com>
2016-11-14 22:21:49 +0100

    Correction de la User Story 1 + Ajout du menu
    La user Story 3 faite par Shule a également été ajoutée dans le fichier
    principal.

--- src/main/java/TSE/P_INFO/CosmopoliTse/DaveHttp.java
+++ src/main/java/TSE/P_INFO/CosmopoliTse/DaveHttp.java
@@ -3,9 +3,7 @@ package TSE.P_INFO.CosmopoliTse;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.HashMap;
-import java.util.Hashtable;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
@@ -21,6 +19,8 @@ import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 
 public class DaveHttp {
+
+	private static Scanner input= new Scanner(System.in);
 
 	//Uncompress the received dates
 	public static String uncompress(GZIPInputStream in) throws IOException {   
@@ -71,51 +71,60 @@ public class DaveHttp {
         return result;
     }
 	
+	
 	public static void firstStory() throws IOException{
-		Scanner in = new Scanner(System.in);
+		input = new Scanner(System.in);
 		System.out.println("Sur quel sujet voulez-vous effectuer la recherche?");
-		String tag = in.nextLine();
-		System.out.println("Combien de contributeurs souhaitez-vous afficher?");
-		int count=in.nextInt();
-		String url = "https://api.stackexchange.com/2.2/tags/" + tag + "/top-answerers/all_time?page=1&pagesize="+count+"&site=stackoverflow";
+		String tag = input.nextLine();
+		System.out.println("Combien de contributeurs voulez-vous afficher? (20 max)");
+		int count=input.nextInt();
+		String url = "https://api.stackexchange.com/2.2/tags/" + tag + "/top-answerers/all_time?page=1&pagesize="+20+"&site=stackoverflow";
 		String jsonString = DaveHttp.sendGet(url);
 		JSONObject obj= new JSONObject(jsonString);
-		List<String> contributorsNames = new ArrayList<String>();
-		List<Long> contributorsScores = new ArrayList<Long>();
-		for(int i=0;i<count;i++)
+		Map<String,Long> contributors = new HashMap<String,Long>();
+		List<Long> contributorsPostCount = new ArrayList<Long>();
+		for(int i=0;i<20;i++)
 		{
 			String nom = obj.getJSONArray("items").getJSONObject(i).getJSONObject("user").getString("display_name");
-			contributorsNames.add(nom);
-			Long score = obj.getJSONArray("items").getJSONObject(i).getLong("score");
-			contributorsScores.add(score);
+			Long score = obj.getJSONArray("items").getJSONObject(i).getLong("post_count");
+			contributorsPostCount.add(score);
+			contributors.put(nom, score);
 		}
 		
+		contributorsPostCount.sort(null);
+	
 		
 		System.out.println("Les meilleurs contributeurs (Nom : Score) au sujet " + tag + " sont: ");
-		for(int i=0;i<contributorsNames.size();i++)
+		for(int i=19;i>=19-(count-1);i--)
 		{
-			System.out.println(contributorsNames.get(i) + ": " + contributorsScores.get(i));
+			for(Map.Entry<String, Long> cont:contributors.entrySet())
+				if(cont.getValue()==contributorsPostCount.get(i))
+				{
+					System.out.println(cont.getKey() + ": " + cont.getValue());
+				}
 		}
+
 	}
 	
-    public static String secondStoryDave(String Tag) throws IOException{
+    public static void secondStoryDave() throws IOException{
+    	System.out.println("Pour quel sujet souhaitez-vous effectuer la recherche?");
+    	String Tag = input.next();
     	String url = "https://api.stackexchange.com/2.2/tags/" + Tag + "/top-answerers/all_time?page=1&pagesize=1&site=stackoverflow";
     	String jsonString = DaveHttp.sendGet(url);
     	JSONObject obj= new JSONObject(jsonString);  
     	String tagTopAnswerer=obj.getJSONArray("items").getJSONObject(0).getJSONObject("user").getString("display_name");
-    	return tagTopAnswerer;
+    	System.out.println("L'utilisateur ayant le Top Tag dans le sujet " + Tag + " est : " + tagTopAnswerer);
     }
     
-    public static String thirdStoryDave() throws IOException{
+    public static void thirdStoryDave() throws IOException{
     	Scanner in = new Scanner(System.in);
     	Scanner in1 = new Scanner(System.in);
     	System.out.println("Commbien de tag vous voulez inserer?");
     	int nombre = in.nextInt();
     	List<String> tagList = new ArrayList<String>();
     	Map<String,Long> userScoreMap = new HashMap<String, Long>();
-    	List <Map<String,Long>> listUserScoreMap = new ArrayList<Map<String,Long>>();
     	for(int i=0;i<nombre;i++){
-    		System.out.println("Inserer le "+(i+1)+" tag");
+    		System.out.println("Inserer le tag "+(i+1));
     		String tags = in1.nextLine();
     		tagList.add(tags);
     	}
@@ -124,64 +133,113 @@ public class DaveHttp {
         	String jsonString = DaveHttp.sendGet(url);
         	JSONObject obj= new JSONObject(jsonString); 
         	for(int j=0;j<20;j++){
+        		int flag=0;
         		String nom = obj.getJSONArray("items").getJSONObject(j).getJSONObject("user").getString("display_name");
-        		Long score = obj.getJSONArray("items").getJSONObject(j).getLong("score");
-        		userScoreMap.put(nom,score);
+        		Long post= obj.getJSONArray("items").getJSONObject(j).getLong("post_count");
+        		for (Map.Entry<String, Long> entry : userScoreMap.entrySet()){
+        			if(entry.getKey().equals(nom)){
+        				entry.setValue(post+entry.getValue());
+        				flag=1;
+        			}break;
+        		}
+        		if(flag==0){
+        			userScoreMap.put(nom,post);
+        		}
         	}
-        	listUserScoreMap.add(userScoreMap);
     	}
-    	for(Map<String,Long> map: listUserScoreMap){
-    		for (String s : map.keySet()) {
-    			System.out.println("nom:" + s);
-    			System.out.println("score:" + map.get(s));
+    	Long max =(long) 0; 
+    	for (Long post : userScoreMap.values()) {  
+    		  if(max <= post){
+    			  max = post;
+    		  }
+    	}  
+    	for (Map.Entry<String, Long> entry1 : userScoreMap.entrySet()){
+    		if(entry1.getValue()==max){
+    			System.out.print("Le personne qui contribue le plus sur les tags: ");
+    			for(String ttt:tagList){
+    				System.out.print("("+ttt+")");
+    			}
+    			System.out.print(" est "+ entry1.getKey());
     		}
-    		System.out.println("---------------");
     	}
-    	
-    	
-    	return "";
     }
     
     public static void menu() throws IOException{
-    	System.out.println("Que souhaitez-vous afficher?");
-    	System.out.println("1: Les personnes contribuant le plus à un sujet donné");
-    	System.out.println("2: La personne qui a le top tag dans un sujet donné");
-    	System.out.println("3: La personne qui contribue le plus à un ensemble de tags donnés");
-    	Scanner in = new Scanner(System.in);
     	
     	boolean continuer=false;
-    	
+    	Scanner in = new Scanner(System.in);
     	do
     	{
-    		int choix=in.nextInt();
+    		System.out.println("Que souhaitez-vous afficher?");
+        	System.out.println("1: Les personnes contribuant le plus à un sujet donné");
+        	System.out.println("2: La personne qui a le top tag dans un sujet donné");
+        	System.out.println("3: La personne qui contribue le plus à un ensemble de tags donnés");
+        	
+    		int choix=input.nextInt();
+    		String reponse;
+    		
 	    	switch (choix)
 	    	{
 	    	  case 1:
 	    		  firstStory();
-	    		  continuer=false;
-	    	    break;  
+	    		  System.out.println();
+	    		  do
+	    		  {
+	    			  System.out.println("Souhaitez-vous effectuer une autre recherche? y/n");
+		    		  reponse=in.next();
+	    			  if(reponse.equals("y"))
+	    				  continuer=true;
+	    			  else if (reponse.equals("n"))
+	    				  continuer=false;  
+	    			  else
+	    				  System.out.println("Choix incorrect.");
+	    		  }while(!(reponse.equals("y") || reponse.equals("n")));
+	    		  break;  
 	    	  case 2:
-	    		  secondStoryDave("java");
-	    		  continuer=false;
+	    		  secondStoryDave();
+	    		  System.out.println();
+	    		  do
+	    		  {
+	    			  System.out.println("Souhaitez-vous effectuer une autre recherche? y/n");
+		    		  reponse=in.next();
+	    			  if(reponse.equals("y"))
+	    				  continuer=true;
+	    			  else if (reponse.equals("n"))
+	    				  continuer=false;  
+	    			  else
+	    				  System.out.println("Choix incorrect.");
+	    		  }while(!(reponse.equals("y") || reponse.equals("n")));
 	    		  break;
 	    	  case 3:
 	    		  thirdStoryDave();
-	    		  continuer=false;
+	    		  System.out.println();
+	    		  do
+	    		  {
+	    			  System.out.println("Souhaitez-vous effectuer une autre recherche? y/n");
+		    		  reponse=in.next();
+	    			  if(reponse.equals("y"))
+	    				  continuer=true;
+	    			  else if (reponse.equals("n"))
+	    			  {
+	    				  System.out.println("Arrêt de l'application...");
+	    				  continuer=false;  
+	    			  }
+	    			  else
+	    				  System.out.println("Choix incorrect.");
+	    		  }while(!(reponse.equals("y") || reponse.equals("n")));
 	    		  break;
 	    	  default:
 	    		  System.out.println("Erreur de choix, veuillez recommencer.");
+	    		  System.out.println();
 	    		  continuer=true;
 	    		  break;
 	    	}
 	    	
     	}while(continuer);
+    	in.close();
     }
     
     public static void main(String[] args) throws IOException {
     	menu();
-    	//firstStory();
-       //System.out.println(DaveHttp.secondStoryDave("javascript"));
-    	//thirdStoryDave();
-    	
     }
 }
